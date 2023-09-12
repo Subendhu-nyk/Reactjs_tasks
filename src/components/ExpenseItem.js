687da64d0508c56145c2d6cc49d0fd48108b1962
Subendhu-nyk <subendhu.nyk@gmail.com>
2023-09-12 20:24:35 +0530

    custom component(task7)

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem(){
+    return (<div>
+        
+        <div><h2>Expense Item!</h2></div>
+        <div><h3>Food</h3> <p>Rs10</p></div>
+        <div><h3>Petrol</h3> <p>Rs100</p></div>
+        <div><h3>Movies</h3> <p>Rs100</p></div>    
+    
+    </div>)
+}
+
+export default ExpenseItem;
