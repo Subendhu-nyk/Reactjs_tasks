8c3ec0c3c9923fd52eeeb4620dfc243e538603eb
Subendhu-nyk <subendhu.nyk@gmail.com>
2023-09-23 23:30:21 +0530

    Conditional Rendering task12

--- src/components/Expenses/ExpenseList.js
+++ src/components/Expenses/ExpenseList.js
@@ -7,18 +7,36 @@ const ExpensesList = (props) => {
   if (props.items.length === 0) {
     return <h2 className='expenses-list__fallback'>Found no expenses.</h2>;
   }
+  
 
   return (
     <ul className='expenses-list'>
-      {props.items.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          locationOfExpenditure={expense.locationOfExpenditure}
-          amount={expense.amount}          
-          date={expense.date}
-        />
-      ))}
+      {props.items.length > 1 ? (
+        <div>
+          {props.items.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              locationOfExpenditure={expense.locationOfExpenditure}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))}
+        </div>
+      ) : (
+        <div>
+          {props.items.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              locationOfExpenditure={expense.locationOfExpenditure}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))}
+          <h2 className='expenses-list__fallback'>Only single Expense here.</h2>
+        </div>
+      )}
     </ul>
   );
 };
