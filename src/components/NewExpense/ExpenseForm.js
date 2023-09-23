ce031c454e3fb33bd0179dd6384ea40a14207c0e
Subendhu-nyk <subendhu.nyk@gmail.com>
2023-09-23 13:49:07 +0530

    Submitting the forms(task 6)

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -8,24 +8,65 @@ function ExpenseForm(){
     const [loc,setLoc]=useState('')
     const [amt,setAmt]=useState('')
     const [date,setDate]=useState('')
+    // const [userInput,setUserInput]=useState({
+    //     enteredItem:"",
+    //     enteredLoc:"",
+    //     enteredAmount:"",
+    //     enteredDate:""
+    // })
     const onclickHandler1=(event)=>{
         setItem(event.target.value)
+        // setUserInput({
+        //     ...userInput,
+        //     enteredItem:event.target.value,
+        // })
+        // setUserInput((prevState)=>{
+        //    return{ ...prevState,
+        //     enteredItem:event.target.value,}
+        // })
     }
     const onclickHandler2=(event)=>{
         setLoc(event.target.value)
+        // setUserInput({
+        //     ...userInput,
+        //     enteredLoc:event.target.value,
+        // })
+        // setUserInput((prevState)=>{
+        //     return{ ...prevState,
+        //         enteredLoc:event.target.value,}
+        //  })
     }
     const onclickHandler3=(event)=>{
         setAmt(event.target.value)
+        // setUserInput({
+        //     ...userInput,
+        //     enteredAmount:event.target.value,
+        // })
+        // setUserInput((prevState)=>{
+        //     return{ ...prevState,
+        //         enteredAmount:event.target.value,}
+        //  })
     }
     const onclickHandler4=(event)=>{
+        // setUserInput((prevState)=>{
+        //     return{ ...prevState,
+        //         enteredDate:event.target.value,}
+        //  })
         setDate(event.target.value)
     }
 
+    const submitForm=(event)=>{
+        event.preventDefault();
+        const expenseData={
+            item,loc,amt,date:new Date(date)
+        }
+       console.log(expenseData)
+    }
 
 
 
     return(<div>
-        <form>
+        <form onSubmit={submitForm}>
             <div className="new-expense__controls">
             <div className="new-expense__control">
             <label>Expense Item</label>
